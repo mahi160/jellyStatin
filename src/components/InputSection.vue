0bc88e19a435f44e162cedd603be778d91fa4b52
Salauddin Omar Sifat <omarsifat288@gmail.com>
2020-02-14 16:31:03 +0600

    On branch master  Initial commit

---
+++ src/components/InputSection.vue
@@ -0,0 +1,192 @@
+<template>
+  <div class="input-section">
+    <!--INPUT CARD SECTION-->
+    <b-container v-show="show">
+      <!--LIGHT MODE-->
+      <b-card
+        v-show="!isDark"
+        bg-variant="light"
+        header="JellyStat"
+        class="text-center mx-auto"
+      >
+        <b-card-text>
+          <b-row>
+            <b-col md="8">
+              <div role="group">
+                <b-form-input
+                  id="input-live"
+                  v-model="serverIP"
+                  :state="serverIPState"
+                  @keyup="$emit('update:serverIP', serverIP)"
+                  aria-describedby="input-live-help input-live-feedback"
+                  placeholder="Jellyfin IP Address"
+                  trim
+                ></b-form-input>
+
+                <b-form-invalid-feedback id="input-live-feedback">
+                  http:// or https:// must be used!
+                </b-form-invalid-feedback>
+              </div>
+            </b-col>
+
+            <b-col md="4">
+              <div role="group">
+                <b-form-input
+                  id="input-live"
+                  type="number"
+                  v-model="serverPort"
+                  :state="serverPortState"
+                  @keyup="$emit('update:serverPort', serverPort)"
+                  aria-describedby="input-live-help input-live-feedback"
+                  placeholder="Port"
+                  trim
+                ></b-form-input>
+              </div>
+            </b-col>
+          </b-row>
+
+          <b-row>
+            <b-col>
+              <div role="group">
+                <b-form-input
+                  id="input-live"
+                  v-model="serverAPI"
+                  :state="serverAPIState"
+                  @keyup="$emit('update:serverApi', serverApi)"
+                  aria-describedby="input-live-help input-live-feedback"
+                  placeholder="Jellyfin API key"
+                  trim
+                ></b-form-input>
+              </div>
+            </b-col>
+          </b-row>
+        </b-card-text>
+        <b-button
+          variant="primary"
+          @click="
+            submit();
+            allStats();
+          "
+          >Show Stats!</b-button
+        >
+      </b-card>
+
+      <!--DARK MODE-->
+      <b-card
+        v-show="isDark"
+        bg-variant="dark"
+        header="JellyStat"
+        text-variant="white"
+        class="text-center mx-auto"
+      >
+        <b-card-text>
+          <b-row>
+            <b-col md="8">
+              <div role="group">
+                <b-form-input
+                  class="form-input"
+                  id="input-live"
+                  v-model="serverIP"
+                  :state="serverIPState"
+                  @keyup="$emit('update:serverIP', serverIP)"
+                  aria-describedby="input-live-help input-live-feedback"
+                  placeholder="Jellyfin IP Address"
+                  trim
+                ></b-form-input>
+
+                <b-form-invalid-feedback id="input-live-feedback">
+                  http:// or https:// must be used!
+                </b-form-invalid-feedback>
+              </div>
+            </b-col>
+
+            <b-col md="4">
+              <div role="group">
+                <b-form-input
+                  class="form-input"
+                  id="input-live"
+                  type="number"
+                  v-model="serverPort"
+                  :state="serverPortState"
+                  @keyup="$emit('update:serverPort', serverPort)"
+                  aria-describedby="input-live-help input-live-feedback"
+                  placeholder="Port"
+                  trim
+                ></b-form-input>
+              </div>
+            </b-col>
+          </b-row>
+
+          <b-row>
+            <b-col>
+              <div role="group">
+                <b-form-input
+                  class="form-input"
+                  id="input-live"
+                  v-model="serverAPI"
+                  :state="serverAPIState"
+                  @keyup="$emit('update:serverApi', serverApi)"
+                  aria-describedby="input-live-help input-live-feedback"
+                  placeholder="Jellyfin API key"
+                  trim
+                ></b-form-input>
+              </div>
+            </b-col>
+          </b-row>
+        </b-card-text>
+
+        <b-button
+          variant="primary"
+          @click="
+            submit();
+            allStats();
+          "
+          >Show Stats!</b-button
+        >
+      </b-card>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "input-section",
+  props: {
+    serverIP: String,
+    serverPort: String,
+    serverAPI: String,
+    show: Boolean,
+    isDark: Boolean
+  },
+  computed: {
+    serverIPState() {
+      return this.serverIP.search("http") ? false : true;
+    },
+    serverPortState() {
+      return this.serverPort.length == 4 ? true : false;
+    },
+    serverAPIState() {
+      return this.serverAPI.length >= 15 ? true : false;
+    }
+  },
+  methods: {
+    submit() {
+      this.$emit("submit");
+    },
+    allStats() {
+      this.$emit("all-stat");
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.card {
+  width: 600px;
+  input {
+    margin-top: 20px;
+    padding: 15px;
+    font-size: 20px;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <input-section
+      :serverIP.sync="serverIP"
+      :serverPort.sync="serverPort"
+      :serverAPI.sync="serverAPI"
+      :isDark="isDark"
+      :show="show"
+    ></input-section>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import InputSection from "@/components/InputSection.vue";
+
+export default {
+  name: "Home",
+  components: {
+    InputSection
+  },
+  data() {
+    return {
+      serverIP: "",
+      serverPort: "",
+      serverAPI: "",
+      isDark: false,
+      show: true
+    };
+  }
+};
+</script>
